76c451a557bcd363ebc90ca5700805e3c98130fa
Orphe650 <davortiz@gmail.com>
2020-03-05 07:17:13 +0100

    songs and filters part

--- controllers/songs.js
+++ controllers/songs.js
@@ -1,6 +1,25 @@
-const router = require('express').Router()
-const db = require('../db')
+const router = require("express").Router();
+const db = require("../db");
 
-router.get('/', (req, res) => {})
+router.get("/", (req, res) => {
+  let query_result = `SELECT songs.name, albums.name as album, artists.name AS artist,
+  genres.name as genre, songs.audio FROM songs
+  LEFT JOIN albums on songs.album = albums.id
+  LEFT JOIN artists on songs.artist = artists.id
+  LEFT JOIN genres on songs.genre = genres.id `;
 
-module.exports = router
+  if (req.query.album) query_result += `WHERE albums.id=${req.query.album}`;
+  if (req.query.artist) query_result += `WHERE artists.id=${req.query.artist}`;
+  if (req.query.genre) query_result += `WHERE genres.id=${req.query.genre}`;
+  db.query(query_result, (err, results) => {
+    // results
+    if (err) {
+      res.send(err);
+    } else {
+      console.log("query", req.query);
+      res.send(results);
+    }
+  });
+});
+
+module.exports = router;
