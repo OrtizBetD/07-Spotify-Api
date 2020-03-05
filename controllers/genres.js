df729c277ddfb1f00c0e93673ac2ee7291d64840
Orphe650 <davortiz@gmail.com>
2020-03-05 05:14:24 +0100

    Genres query

--- controllers/genres.js
+++ controllers/genres.js
@@ -1,8 +1,26 @@
-const router = require('express').Router()
-const db = require('../db')
+const router = require("express").Router();
+const db = require("../db");
 
-router.get('/', (req, res) => {})
+router.get("/", (req, res) => {
+  db.query(`SELECT * FROM genres`, (err, results) => {
+    // results
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(results);
+    }
+  });
+});
 
-router.get('/:id', (req, res) => {})
+router.get("/:id", (req, res) => {
+  db.query(`SELECT * FROM genres where id=${req.params.id}`, (err, results) => {
+    // results
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(results);
+    }
+  });
+});
 
-module.exports = router
+module.exports = router;
