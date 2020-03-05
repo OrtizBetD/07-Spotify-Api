f5c877039989e4828d5da340cc3456d3a27e56e8
Orphe650 <davortiz@gmail.com>
2020-03-05 05:33:44 +0100

    Albums and Songs part

--- controllers/albums.js
+++ controllers/albums.js
@@ -1,8 +1,34 @@
-const router = require('express').Router()
-const db = require('../db')
+const router = require("express").Router();
+const db = require("../db");
 
-router.get('/', (req, res) => {})
+router.get("/", (req, res) => {
+  db.query(
+    `SELECT albums.name AS album, artists.name AS artist, albums.cover
+    FROM albums LEFT JOIN artists on albums.artist = artists.id`,
+    (err, results) => {
+      // results
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(results);
+      }
+    }
+  );
+});
 
-router.get('/:id', (req, res) => {})
+router.get("/:id", (req, res) => {
+  db.query(
+    `SELECT albums.name AS album, artists.name AS artist, albums.cover
+  FROM albums LEFT JOIN artists on albums.artist = artists.id where albums.id=${req.params.id}`,
+    (err, results) => {
+      // results
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(results);
+      }
+    }
+  );
+});
 
-module.exports = router
+module.exports = router;
--- controllers/artists.js
+++ controllers/artists.js
@@ -1,8 +1,29 @@
-const router = require('express').Router()
-const db = require('../db')
+const router = require("express").Router();
+const db = require("../db");
 
-router.get('/', (req, res) => {})
+router.get("/", (req, res) => {
+  db.query(`SELECT * FROM artists`, (err, results) => {
+    // results
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(results);
+    }
+  });
+});
 
-router.get('/:id', (req, res) => {})
+router.get("/:id", (req, res) => {
+  db.query(
+    `SELECT * FROM artists where id=${req.params.id}`,
+    (err, results) => {
+      // results
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(results);
+      }
+    }
+  );
+});
 
-module.exports = router
+module.exports = router;
