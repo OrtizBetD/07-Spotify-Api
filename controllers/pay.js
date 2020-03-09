58df4dacbb51c1e22178ea751517bdf5fc0fee37
Orphe650 <davortiz@gmail.com>
2020-03-09 02:25:07 +0100

    update for pay controller

--- controllers/pay.js
+++ controllers/pay.js
@@ -1,5 +1,21 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const stripe = require("stripe")(process.env.STRIPE_SK);
 
-router.post('/', (req, res) => {})
+router.post("/", (req, res) => {
+  stripe.charges
+    .create({
+      amount: 999,
+      currency: "usd",
+      description: "payment",
+      source: req.body.token
+    })
+    .then(res => {
+      console.log(res);
+      res.send({ sucess: true });
+    })
+    .catch(err => {
+      res.send({ sucess: false });
+    });
+});
 
-module.exports = router
+module.exports = router;
